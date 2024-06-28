282e9c1a488157d1b23d7be6715c2dd4cbdf687f
S. Wilkerson <shelcicky@yahoo.com.br>
2024-06-28 19:20:41 -0300

    [001]: Added cypress config and package.json;

---
+++ cypress.config.js
@@ -0,0 +1,12 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+
+    },
+    baseUrl: 'https://www.saucedemo.com',
+    defaultCommandTimeout: 8000,
+    chromeWebSecurity: false
+  },
+});
